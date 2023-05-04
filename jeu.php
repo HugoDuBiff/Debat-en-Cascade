35b77995a4180daf572b5bcddd9ab6ec9a31c189
HugoDuBiff <113457039+HugoDuBiff@users.noreply.github.com>
2023-05-04 16:45:40 +0200

    Add files via upload

--- jeu.php
+++ jeu.php
@@ -1,89 +1,48 @@
-Skip to content
-Search or jump to…
-Pulls
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@HugoDuBiff 
-HugoDuBiff
-/
-Debat-en-Cascade
-Public
-Cannot fork because you own this repository and are not a member of any organizations.
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-More
-Beta Try the new code view
-Debat-en-Cascade/jeu.php /
-@HugoDuBiff
-HugoDuBiff Create jeu.php
-Latest commit b7efd85 45 minutes ago
- History
- 1 contributor
-42 lines (39 sloc)  1.3 KB
- 
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Mon jeu</title>
-	<link rel="stylesheet" href="style.css">
+	<title>Débat en Cascade</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<?php
-	// Vérifier si le formulaire a été soumis
-	if (isset($_POST['submit'])) {
-		// Récupérer le nombre de joueurs et le débat à partir du formulaire
-		$nb_joueurs = $_POST['nb_joueurs'];
-		$debat = $_POST['debat'];
+	<div class="container">
+		<h1>Bienvenue sur le jeu de débat</h1>
+		<?php
+			// Récupération des données du formulaire
+			$nb_joueurs = $_GET['nbJoueurs'];
+			$debat = $_GET['debat'];
+			// Récupération du nombre de joueurs et du débat
+			// Création d'un tableau contenant le débat et une seule occurrence de "Tu es le fumier"
+			$cartes = array_merge(array_fill(0, $nb_joueurs - 1, $debat), ["Tu es le fumier"]);
 
-		// Générer une carte aléatoire pour chaque joueur
-		$cartes = array("carte1.png", "carte2.png", "carte3.png", "carte4.png");
-		shuffle($cartes);
+			// Mélange aléatoire du tableau
+			shuffle($cartes);
 
-		// Afficher les cartes pour chaque joueur
-		for ($i = 1; $i <= $nb_joueurs; $i++) {
-			echo "<div class='carte'>";
-			echo "<img src='images/" . $cartes[$i-1] . "'>";
-			echo "<form method='post' action=''>";
-			echo "<input type='text' name='joueur_" . $i . "' placeholder='Nom du joueur'>";
-			echo "<input type='hidden' name='carte_" . $i . "' value='" . $cartes[$i-1] . "'>";
-			echo "</form>";
-			echo "</div>";
-		}
-	} else {
-		// Afficher le formulaire pour demander le nombre de joueurs et le débat
-		echo "<form method='post' action=''>";
-		echo "<label>Nombre de joueurs:</label>";
-		echo "<input type='number' name='nb_joueurs' min='1' max='10'>";
-		echo "<label>Débat:</label>";
-		echo "<input type='text' name='debat'>";
-		echo "<input type='submit' name='submit' value='Jouer'>";
-		echo "</form>";
-	}
-	?>
 
+
+
+			// Affichage des cartes
+			echo '<div id="cartes">';
+			for ($i=0; $i<$nb_joueurs; $i++) {
+				echo '<div class="carte" data-carte="'.$cartes[$i].'">Carte '.($i+1).'</div>';
+			}
+			echo '</div>';
+		?>
+
+		<script>
+			var cartes = document.querySelectorAll('.carte');
+			cartes.forEach(function(carte) {
+				carte.addEventListener('click', function() {
+					var carte_selectionnee = this.getAttribute('data-carte');
+					if (carte_selectionnee == "Tu es le fumier") {
+						this.innerHTML = "<p>Tu es le fumier</p>";
+					} else {
+						this.innerHTML = "<p>"+carte_selectionnee+"</p>";
+					}
+				});
+			});
+		</script>
+	</div>
 </body>
 </html>
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Debat-en-Cascade/jeu.php at main · HugoDuBiff/Debat-en-Cascade
